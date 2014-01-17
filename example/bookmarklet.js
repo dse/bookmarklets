6ad375cf547e3928e8867a9f457166eef9b36027
Darren Embry <dse@webonastick.com>
2014-01-16 21:52:34 -0500

    how about a bookmarklet?

---
+++ example/bookmarklet.js
@@ -0,0 +1,9 @@
+/**************** BOOKMARKLET BEGINS ON NEXT LINE ****************/false?
+javascript:
+(function(){
+	var h = document.getElementsByTagName("head")[0];
+	var s = document.createElement("script");
+	s.src = "https://raw.github.com/dse/bookmarklets/master/example/example.js";
+	h.insertBefore(s, h.childNodes[0]);
+	window.__run__();
+}());
