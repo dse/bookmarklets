f89732f2b76a2dfb5d668bb6449d16742f97ad35
Darren Embry <dse@webonastick.com>
2014-01-16 21:52:42 -0500

    how about an example?

---
+++ example/example.js
@@ -0,0 +1,7 @@
+/*jslint browser: true, sloppy: true */
+//-----------------------------------------------------------------------------
+// Lines above are for jslint, the JavaScript verifier.  http://www.jslint.com/
+//-----------------------------------------------------------------------------
+function __run__() {
+	console.log("yay");
+}
